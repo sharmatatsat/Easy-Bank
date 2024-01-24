43fa5b22bd7cfa65a57453b740885a229828d241
sharmatatsat23 <sharmatatsat23@gmail.com>
2024-01-24 15:02:18 +0530

    added transaction page

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const user = {
-
-   userName : sharmatatsat,
-   pass : bankist23 ,
+   name : 'Tatsat',
+   userName : 'sharmatatsat',
+   pass : 'bankist23' ,
+   pin : '1234',
 
 };
 
 const redirect = () =>{
 
-    if(document.querySelector('#userName').value === user.userName && document.querySelector('#pass').value === user.pass){
-    window.location.href('bankPage.html')
+    const enteredUsername = document.querySelector('#userName').value;
+    const enteredPassword = document.querySelector('#pass').value
+    
+    if( enteredUsername === user.userName &&  enteredPassword=== user.pass){
+        window.location.href = "http://127.0.0.1:5500/Applications/Bankist/bankPage.html";
 
     }
+
+    else{
+        console.log("Incorrect credentials !")
+    }
+
+    console.log("redirect fnction called")
 }
 const currentDate = new Date();
 const hours = currentDate.getHours();
+
 
 const greet = () => {
    let greetings = document.createElement('h1');
@@ -21,9 +32,37 @@ const greet = () => {
 
    const mainGreet = document.querySelector('.greet');
    mainGreet.appendChild(greetings);
-
+   
+   const typeGreet = document.querySelector('.greeting'); 
+        
+   let time;
     
-    if(hours<12){
-         
+   if(hours<12 && hours > 0){
+        time = "Morning";
     }
+
+    else if(hours >=12 && hours <=16){
+        time = "Afternoon";
+    }
+
+    else{
+        time = "Evening";
+    }
+
+    typeGreet.innerHTML = `Good ${time}, ${user.name}`;
+}
+
+greet();
+
+
+const bankStatement = () => {
+
+    let list = document.createElement("li");
+    const amountList = document.querySelector(".record-list");
+    amountList.appendChild(list);
+    list.classList.add("innerList");
+    let transaction = "withdrawal";
+    list.innerHTML = transaction;
+
+    // console.log("its working");
 }
