e4a815c3cbec14fdcbe67dbcf5746bf1a7573723
sharmatatsat23 <sharmatatsat23@gmail.com>
2024-01-28 21:56:31 +0530

    added al frnt-end functionalities

--- app.js
+++ app.js
@@ -17,44 +17,48 @@ const user = [
     },
 
 ];
-    
-    const currentDate = new Date();
-    const hours = currentDate.getHours();
-    let minutes = currentDate.getMinutes();
-    let newBalance,amountList, balance, amountBalance , inAmount = 0,outAmount =0;
-    const date = currentDate.getDate();
-    const month = currentDate.getMonth()+1;
-    const year = currentDate.getFullYear();
-    const fullDay = `${date}/${month}/${year}`;
-    
 
-const foundUser = function(){
+const currentDate = new Date();
+const hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let newBalance, amountList, balance, amountBalance, inAmount = 0, outAmount = 0;
+const date = currentDate.getDate();
+const month = currentDate.getMonth() + 1;
+const year = currentDate.getFullYear();
+const fullDay = `${date}/${month}/${year}`;
+
+
+const foundUser = function () {
     const enteredUsername = document.querySelector('#userName').value;
     const enteredPassword = document.querySelector('#pass').value;
     const loggedInUser = user.find(u => enteredUsername === u.userName && enteredPassword === u.pass);
-    
+
 
     return loggedInUser;
 }
 
 
-const redirect = () =>{
-    
+const redirect = () => {
+
     if (foundUser()) {
         console.log(foundUser());
-        localStorage.setItem('users',JSON.stringify(foundUser()));
+        localStorage.setItem('users', JSON.stringify(foundUser()));
         window.location.href = "http://127.0.0.1:5500/bankPage.html";
-            
-        }
         
-        console.log("redirect fnction called")
+    }
+    
+    else{
+        alert("Invalid Credentials !")
+        removeInput()
+    }
+    console.log("redirect fnction called")
     
 }
 
 
-if(minutes <=9){
+if (minutes <= 9) {
     minutes = `0${minutes}`;
-
+    
 }
 
 let time = `${hours}:${minutes}`
@@ -64,65 +68,65 @@ console.log(time);
 const accountInfo = () => {
     let accUser = JSON.parse(localStorage.getItem('users'));
     const availableAmount = document.querySelector("#amount");
-    if(accUser.Nationality === 'Indian'){
+    if (accUser.Nationality === 'Indian') {
         availableAmount.innerHTML = `INR ${accUser.InitialAmount}`
     }
-
-    timeOfTransaction();
-} 
-
-
-    const greet = () => {
-
-        let storedUser = JSON.parse(localStorage.getItem('users'));
-        console.log(storedUser);
-        let greetings = document.createElement('h1');
-        greetings.classList.add('greeting');
-     
-        const mainGreet = document.querySelector('.greet');
-        mainGreet.appendChild(greetings);
-        
-        const typeGreet = document.querySelector('.greeting'); 
-             
-        let time;
-         
-        if(hours<12 && hours > 0){
-             time = "Morning";
-         }
-     
-         else if(hours >=12 && hours <=16){
-             time = "Afternoon";
-         }
-     
-         else{
-             time = "Evening";
-         }
-       
-        typeGreet.innerHTML = `Good ${time}, ${storedUser.name}`;
-        console.log("working greetFunc");
-             
-     }
-
     
+    timeOfTransaction();
+}
+
+
+const greet = () => {
+    
+    let storedUser = JSON.parse(localStorage.getItem('users'));
+    console.log(storedUser);
+    let greetings = document.createElement('h1');
+    greetings.classList.add('greeting');
+    
+    const mainGreet = document.querySelector('.greet');
+    mainGreet.appendChild(greetings);
+    
+    const typeGreet = document.querySelector('.greeting');
+    
+    let time;
+    
+    if (hours < 12 && hours > 0) {
+        time = "Morning";
+    }
+    
+    else if (hours >= 12 && hours <= 16) {
+        time = "Afternoon";
+    }
+    
+    else {
+        time = "Evening";
+    }
+    
+    typeGreet.innerHTML = `Good ${time}, ${storedUser.name}`;
+    console.log("working greetFunc");
+    
+}
+
+
 
 
 const bankStatementTransfer = () => {
     
     
-        
-       
-            let list = document.createElement("li");
-            const amountList = document.querySelector(".record-list");
-            amountList.appendChild(list);
-            list.classList.add("innerList");
-            list.classList.add("bgColorRed");
-            let transaction = "Debited";
-            list.innerHTML = transaction; 
-        
-            AmountTransact();   
-
-
-        
+    
+    
+    let list = document.createElement("li");
+    const amountList = document.querySelector(".record-list");
+    amountList.appendChild(list);
+    list.classList.add("innerList");
+    list.classList.add("bgColorRed");
+    let transaction = "Debited";
+    list.innerHTML = transaction;
+    
+    AmountTransact();
+    
+    
+    
 }
 const bankStatementLoan = () => {
     
@@ -138,26 +142,29 @@ const bankStatementLoan = () => {
 
 
 const AmountTransact = () => {
+    let loggedUserTransfer = JSON.parse(localStorage.getItem('users'));
     amountList = document.createElement("li");
     balance = parseFloat(document.querySelector("#transferAmount").value);
     amountBalance = document.querySelector(".amount-transaction");
     amountList.classList.add("balanceList");
     amountBalance.appendChild(amountList);
-
-    newBalance = user.InitialAmount - balance;
     
-    if(newBalance < 0){
+    newBalance = loggedUserTransfer.InitialAmount - balance;
+    
+    if (newBalance < 0) {
         alert("Insufficient Balance");
     }
     
     amountList.innerHTML = `INR ${balance}`;
-
-    user.InitialAmount = newBalance;
+    
+    loggedUserTransfer.InitialAmount = newBalance;
+    localStorage.setItem('users', JSON.stringify(loggedUserTransfer));
     console.log("Amount Transaction");
-    accountInfo(); 
+    accountInfo();
 }
 
 const AmountLoan = () => {
+    let loggedUserLoan = JSON.parse(localStorage.getItem('users'));
     amountList = document.createElement("li");
     balance = parseFloat(document.querySelector("#loanAmount").value);
     amountBalance = document.querySelector(".amount-transaction");
@@ -170,15 +177,17 @@ const AmountLoan = () => {
         return;
     }
     
-    newBalance = user.InitialAmount + balance;
+    newBalance = loggedUserLoan.InitialAmount + balance;
     
     amountList.innerHTML = `INR ${balance}`;
     
-    user.InitialAmount = newBalance;
+    loggedUserLoan.InitialAmount = newBalance;
+    
+    localStorage.setItem('users', JSON.stringify(loggedUserLoan));
     
     // document.querySelector(".innerList").style.background = 'green';
     console.log("Amount Transaction");
-    accountInfo(); 
+    accountInfo();
 }
 
 
@@ -187,21 +196,21 @@ const incomeInfo = () => {
     
     let income = parseFloat(document.querySelector('#loanAmount').value);
     inAmount = parseFloat(inAmount);
-
+    
     inAmount += income;
+    intake.innerHTML = `IN : ${inAmount} ₹`
     interest();
-    intake.innerHTML = `IN : ${inAmount}INR`
 }
 const expenseInfo = () => {
     const outtake = document.querySelector('#OUT');
-
+    
     
     let expense = parseFloat(document.querySelector('#transferAmount').value);
     outAmount = parseFloat(outAmount);
-   
-
+    
+    
     outAmount += expense;
-    outtake.innerHTML = `OUT : ${outAmount}INR`
+    outtake.innerHTML = `OUT : ${outAmount} ₹`
 }
 
 const clearInput = () => {
@@ -213,33 +222,33 @@ const timeOfTransaction = () => {
     timeToday.innerHTML = `As of ${fullDay}, ${time}`;
 }
 const interest = () => {
-    const Interest = (inAmount/100) * 8;    
+    const Interest = (inAmount / 100) * 8;
     return Interest;
 }
 
 const DisplayInterest = () => {
     const interstDis = document.querySelector('#Interest');
     let interestCal = interest();
-
-    interstDis.innerHTML= `INTEREST : ${interestCal}INR`;
-
+    
+    interstDis.innerHTML = `INTEREST : ${interestCal} ₹`;
+    
 }
 
 const logOut = () => {
     let minutes = 9;
     let seconds = 59;
     
-
+    
     const updateTimer = () => {
         console.log(`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
     };
-
+    
     const timerId = setInterval(() => {
         updateTimer();
-
+        
         if (seconds === 0 && minutes === 0) {
             clearInterval(timerId);
-            window.location.href = "http://127.0.0.1:5500/Applications/Bankist/#";
+            window.location.href = "http://127.0.0.1:5500/index.html";
         } else if (seconds === 0) {
             minutes--;
             seconds = 59;
@@ -247,32 +256,49 @@ const logOut = () => {
             seconds--;
         }
     }, 1000);
-
+    
     const timerDisplay = document.querySelector('#loggedOut');
     
     const updateTimerDisplay = () => {
         timerDisplay.innerHTML = `You will be logged out in ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
     };
-
+    
     const displayTimerId = setInterval(updateTimerDisplay, 1000);
-
+    
     const clearDisplayTimer = () => {
         clearInterval(displayTimerId);
     };
-
+    
     // Clear the display timer when the user logs out
     // document.querySelector('#logoutButton').addEventListener('click', clearDisplayTimer);
 };
 
-
-
-const TransferTo = () => {
-    const transferRec = document.querySelector('#transferRecipient').value;
-
-   
+const closeAccount = () => {
+    let userAccount = JSON.parse(localStorage.getItem('users'));
+    const closeAccPin = document.querySelector('#pin').value;
+    const closeAccUsername = document.querySelector('#user_name').value;
+    
+    if(closeAccUsername == userAccount.userName && closeAccPin == userAccount.pin){
+        console.log(`${userAccount.name} account closed !`)
+        userAccount = user.filter(u => u.pin !== userAccount.pin);
+        console.log(userAccount);
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    }
+    
+    else{
+        alert("Invalid Credentials !")
+    }
 }
 
 
+const removeInput = () => {
+    let enterUsername = document.querySelector('#userName');
+    let enterPassword = document.querySelector('#pass');
+    if (enterUsername.value != "")
+        enterUsername.value = "";
+    if (enterPassword.value != "")
+        enterPassword.value = "";
+};
 
 greet();
 accountInfo();
